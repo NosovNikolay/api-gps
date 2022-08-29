436b9d088e753156dd05855fc8587410fbcd4712
NosovNikolay <nosov2205@gmail.com>
2022-08-29 10:15:06 +0200

    fix: move db creds to .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,23 @@ import {GpsModule} from './gps/gps.module';
 import {AuthModule} from "./auth/auth.module";
 import {DevicesModule} from './devices/devices.module';
 import {Device} from "./devices/devices.model";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
     imports: [
         AppModule,
         GpsModule,
         AuthModule,
+        ConfigModule.forRoot({
+            envFilePath: '.env'
+        }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'root',
-            database: 'gps',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database:  process.env.POSTGRES_DB,
             models: [Device],
             autoLoadModels: true
         }),
--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api/docs', app, document);
     app.useGlobalPipes(new ValidationPipe())
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
 
 }
 
